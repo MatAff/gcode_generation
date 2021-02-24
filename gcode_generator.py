dec0dea56af73e7a59cf9c88c21aca483f6cdc44
MatAff <mathijsaffourtit@gmail.com>
2021-02-23 23:51:43 -0700

    consolidate drill and cut methods

--- gcode_generator.py
+++ gcode_generator.py
@@ -15,13 +15,11 @@ G90
 
 FILE_END = "M30"
 
+plunge_feedrate = 250
 rapid_feedrate = 6000
-drill_feedrate = 250
-pullback_depth = 3.0
-pullback_height = 1.0
-clear_height = 5.0
-cut_depth = 1.0
 cut_feedrate = 900
+cut_depth = 1.0
+clear_height = 5.0
 
 
 def inch(i):
@@ -33,6 +31,10 @@ def create_rectangle(width, height):
     return [c * np.array([width, height]) for c in corners]
 
 
+def transpose(lines_list):
+    return [[[p[1], p[0]] for p in line] for line in lines_list]
+
+
 def cut_things(cut_list, depth):
 
     gc = FILE_START
@@ -40,17 +42,11 @@ def cut_things(cut_list, depth):
     # checks
     assert depth > 0, "depth should be given as a positive number"
 
-    # loop and drill
+    # loop and cut or drill
     for cut in cut_list:
 
         # lift
-        gc += f"G1 Z{clear_height} F{drill_feedrate} \n"
-
-        # move to start
-        gc += f"G0 X{cut[0][0]} Y{cut[0][1]} F{rapid_feedrate} \n"
-
-        # zero
-        gc += f"G1 Z{0.0} F{drill_feedrate} \n"
+        gc += f"G1 Z{clear_height} F{plunge_feedrate} \n"
 
         # cut - divide depth in incremental steps
         nr_cycles = math.ceil(depth / cut_depth)
@@ -64,63 +60,13 @@ def cut_things(cut_list, depth):
                 # on first point plunge
                 if ind == 0: 
                     sub_depth = min ((i + 1) * cut_depth, depth)
-                    gc += f"G1 Z-{sub_depth} F{drill_feedrate} \n"
+                    gc += f"G1 Z-{sub_depth} F{plunge_feedrate} \n"
 
             # lift
-            gc += f"G1 Z{clear_height} F{drill_feedrate} \n"
+            gc += f"G1 Z{clear_height} F{plunge_feedrate} \n"
 
     # return to start
-    gc += f"G1 Z{clear_height} F{drill_feedrate} \n"
-    gc += f"G0 X0.0 Y0.0 F{rapid_feedrate} \n"
-
-    # end file
-    gc += FILE_END
-
-    return gc
-
-
-def drill_holes(hole_list, depth):
-    # TODO: adapt cut_lines to incorperate drilling holes, as the process is very similar
-
-    gc = FILE_START
-
-    # checks
-    assert depth > 0, "depth should be given as a positive number"
-
-    # determine hole order
-    # TODO: implement hold ordering
-
-    # loop and drill
-    for hole in hole_list:
-
-        # lift
-        gc += f"G1 Z{clear_height} F{drill_feedrate} \n"
-
-        # move
-        gc += f"G0 X{hole[0]} Y{hole[1]} F{rapid_feedrate} \n"
-
-        # zero
-        gc += f"G1 Z{0.0} F{drill_feedrate} \n"
-
-        # drill - divide depth in incremental steps
-        nr_cycles = math.ceil(depth / pullback_depth)
-        for i in range(nr_cycles):
-            sub_depth = min ((i + 1) * pullback_depth, depth)
-
-            # drill 
-            gc += f"G1 Z-{sub_depth} F{drill_feedrate} \n"
-
-            # wait
-            gc += "G4 S0.5 \n"
-
-            # pull 
-            gc += f"G1 Z{0.0} F{drill_feedrate} \n"
-
-        # lift
-        gc += f"G1 Z{clear_height} F{drill_feedrate} \n"
-
-    # return to start
-    gc += f"G1 Z{clear_height} F{drill_feedrate} \n"
+    gc += f"G1 Z{clear_height} F{plunge_feedrate} \n"
     gc += f"G0 X0.0 Y0.0 F{rapid_feedrate} \n"
 
     # end file
--- spindle_holder_plate.py
+++ spindle_holder_plate.py
@@ -1,41 +1,18 @@
 
 import numpy as np
 
-from gcode_generator import drill_holes, cut_things, inch, create_rectangle
+from gcode_generator import inch, create_rectangle, transpose
+from gcode_generator import cut_things
 
 # --- mounting holes - 1/4 inch drill ---
 
 # settings
-filename = './gcode/spindle_holder_plate_mounting_holes.nc'
+filename = './gcode/spindle_holder_plate.nc'
 
 offset_x = 15 # mm
 offset_y = 40 # mm
 width = 70 # mm apart
 height = 40 # mm apart
-
-# create hole grid
-holes = create_rectangle(width, height)
-print(holes)
-
-# add offset
-holes = holes + np.array([offset_x, offset_y])
-
-# swap x and y
-holes[:, [1, 0]] = holes[:, [0, 1]]
-print(holes)
-
-# generate and preview
-gc = drill_holes(holes, inch(0.5))
-print(gc)
-
-# write to file
-with open(filename, 'w') as file:
-    file.write(gc)
-
-# --- clamp slots and outline - 1/4 router bit ---
-
-# settings
-filename = './gcode/spindle_holder_plate_clamp_outline.nc'
 
 # clamp slot settings
 clamp_width = 65
@@ -48,12 +25,24 @@ clamp_slot_length = 15
 overall_width = offset_x + width + offset_x + inch(0.25 / 2)
 overall_height = offset_y + height + offset_y + inch(0.25 / 2)
 
+# --- mounting holes ---
+
+# create hole grid
+holes = create_rectangle(width, height) + np.array([offset_x, offset_y])
+
+# convert holes to lines with a single point
+hole_lines = [[list(h)] for h in holes]
+
+# --- clamp slots ---
+
 # create clamp slot lines
 clamp_offset_start = np.array([clamp_offset_x, clamp_offset_y])
 clamp_offset_end = np.array([clamp_offset_x, clamp_offset_y + clamp_slot_length])
 slot_start = create_rectangle(clamp_width, clamp_height) + clamp_offset_start
 slot_end = create_rectangle(clamp_width, clamp_height) + clamp_offset_end
 clamp_lines = list(zip(slot_start, slot_end))
+
+# --- outline ---
 
 # create outline
 outline = [
@@ -62,9 +51,13 @@ outline = [
     [0, overall_height],
 ]
 
-# create lines list
-lines_list = [*clamp_lines, outline]
-print(lines_list)
+# --- finalize ---
+
+# join lines list
+lines_list = [*hole_lines, *clamp_lines, outline]
+
+# transpose (swap) x and y
+lines_list = transpose(lines_list)
 
 # generate gcode 
 gc = cut_things(lines_list, inch(0.5))
